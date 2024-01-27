1584e8cd70b06e3095880d5e40028ee44ec7675d
Weilence <weilence@163.com>
2024-01-27 15:37:03 +0800

    chore: Suspense add fallback

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -5,5 +5,10 @@ import NoteList from '@/components/NoteList.vue'
 <template>
   <Suspense>
     <NoteList />
+    <template #fallback>
+      <div class="flex justify-center items-center h-12">
+        <v-progress-circular indeterminate />
+      </div>
+    </template>
   </Suspense>
 </template>
